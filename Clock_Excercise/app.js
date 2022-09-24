d1e845e3e2c09e14efbd720174b384616e543e68
August <Augusthauerslev@outlook.com>
2022-09-24 14:52:11 +0200

    metronome can be hardcoded

--- Clock_Excercise/app.js
+++ Clock_Excercise/app.js
@@ -1,5 +1,4 @@
 import express from "express"
-import { type } from "os";
 import path from "path"
 const app = express();
 //idea: Depending on time the background will change
@@ -7,28 +6,26 @@ const app = express();
 
 app.use(express.static("public"))
 
-
-
 app.get("/", (req,res)=>{
     res.sendFile(path.resolve("public/frontpage.html"))
-    console.log(intervalFunc)
-    res.send({ data: [intervalFunc] });
 })
 
-function intervalFunc() {
-    let hour = new Date().getHours()
-let min = new Date().getMinutes()
-let sec = new Date().getSeconds()
-let time = hour +":"+ min+":" + sec
-    console.log(time);
-  }
 
-setInterval(intervalFunc, 1000); 
-
-
-app.get("/", (req, res) => {
-  
+app.get('/time', (req, res) => {
+    const date = new Date();
+      res.send({
+        hour: date.getHours(),
+        min : date.getMinutes(),
+        sec: date.getSeconds(),
+      })
 });
+
+app.get('/metronome',(req,res)=>{
+    res.send({
+        metronome: bps
+    })
+})
+        
 
 app.listen(8080,()=>{
     console.log("Server is running on ", 8080)
--- Clock_Excercise/public/clocks.js
+++ Clock_Excercise/public/clocks.js
@@ -1,5 +1,39 @@
-console.log("Hello world")
+let clock = document.getElementById("clock");
+let start = document.getElementById("start");
+let bpm = document.getElementById("bpm");
+let hardcodedBPM = 60;
 
-fetch("/pokemon")
-.then(response => response.json())
-.then(result => console.log(result));
+/*
+function getTime(){
+fetch("/time")
+.then(resolve => resolve.json())
+.then(res => {
+    let hour = res.hour
+    let min = res.min
+    let sec = res.sec
+    console.log(sec)
+
+    document.querySelector("#clock").textContent = hour + ":" + min + ":" + sec 
+})};
+*/
+
+function calculateBPS(bpm){
+document.getElementById("bpm").textContent=bpm;
+let bps = 60/bpm
+return bps*1000
+}
+
+function playSound(){
+    var audio = new Audio('tick.wav');
+    audio.play()
+}
+
+function startMetronome(){
+let bps = calculateBPS(120)
+console.log(bps)
+
+setInterval(playSound,bps)
+}
+
+startMetronome();
+//setInterval(getTime, 1000);
