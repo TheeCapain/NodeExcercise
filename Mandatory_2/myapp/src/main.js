0b8270467d94bf27b5e030d480186b3c8e9170c7
August <Augusthauerslev@outlook.com>
2022-11-11 14:15:16 +0100

    Started mandatory 2

---
+++ Mandatory_2/myapp/src/main.js
@@ -0,0 +1,8 @@
+import './svelte/app.css'
+import App from './svelte/login.svelte'
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
---
+++ Mandatory_2/myapp/src/svelte/login.svelte
@@ -0,0 +1,33 @@
+<script>
+	//Implimentering af login forsøg skal laves her
+</script>
+<main>
+<body>
+  <link rel="stylesheet" href="app.css">
+	<div class="main">  	
+		<div>
+			<label for="wth" class="site-titel">Weather App</label>
+		</div>
+		<input type="checkbox" id="chk" aria-hidden="true">
+
+			<div class="signup">
+				<form>
+					<label for="chk" aria-hidden="true">Sign up</label>
+					<input type="text" name="txt" placeholder="User name" required>
+					<input type="email" name="email" placeholder="Email" required>
+					<input type="password" name="pswd" placeholder="Password" required>
+					<button>Sign up</button>
+				</form>
+			</div>
+
+			<div class="login">
+				<form>
+					<label for="chk" aria-hidden="true">Login</label>
+					<input type="email" name="email" placeholder="Email" value="test@mail.com" required>
+					<input type="password" name="pswd" placeholder="Password" value="123" required>
+					<button>Login</button>
+				</form>
+			</div>
+	</div>
+</body>
+</main>
