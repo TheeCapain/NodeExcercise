60a973bafac226c40ba1ae69d0d3b8fd44f116ed
August <Augusthauerslev@outlook.com>
2022-11-19 12:19:38 +0100

    added things

--- Mandatory_2/server/routers/userRouter.js
+++ Mandatory_2/server/routers/userRouter.js
@@ -1,10 +1,11 @@
 import {Router} from 'express'
+import db from "../database/connection_sqlite.js"
 
 const userRouter = Router()
 
 
-userRouter.post("/signup",(req, res)=>{
-
+userRouter.post("/signup", async (req, res)=>{
+console.log("Vi er i router")
         const user = user.find(
             user => 
             user.email === req.body.email &&
@@ -13,7 +14,9 @@ userRouter.post("/signup",(req, res)=>{
 
 if(user){
     console.log("Bruger logget ind")
+    result = await db.run((`INSERT INTO users (user_mail, user_pswd) VALUES (?, ?)`, [latinName, commonName]));
     res.send({data:user.userId})
+
 } else{
     res.send("Wrong login")
 }
