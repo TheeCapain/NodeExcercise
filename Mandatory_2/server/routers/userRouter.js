e70475a7d9b767039b46021f756284cafd8c5377
August <Augusthauerslev@outlook.com>
2022-11-22 17:24:54 +0100

    man kan logge ind

--- Mandatory_2/server/routers/userRouter.js
+++ Mandatory_2/server/routers/userRouter.js
@@ -1,19 +1,14 @@
 import { Router } from 'express'
-import db from "../connection_sqlite.js"
+import db from "../database/connection_sqlite.js"
 
 const userRouter = Router()
 
 userRouter.post("/signup", async (req, res) => {
     console.log("Vi er i router")
-    const user = user.find(
-        user =>
-            user.email === req.body.email &&
-            user.pswd === req.body.pswd
-    );
 
     if (user) {
-        console.log("Bruger logget ind")
-        result = await db.run((`INSERT INTO users (user_mail, user_pswd) VALUES (?, ?)`, [latinName, commonName]));
+        console.log("Bruger oprettet")
+        result = await db.run((`INSERT INTO users (user_mail, user_pswd) VALUES (?, ?)`, [req.body.email, req.body.pswd]));
         res.send({ data: user.userId })
 
     } else {
@@ -21,15 +16,22 @@ userRouter.post("/signup", async (req, res) => {
     }
 })
 
-userRouter.post("/login", async (req, res, next) => {
+userRouter.get("/api/users", async(req,res)=>{
+    const data = await db.all("SELECT * FROM users;");
+    res.send({ data });
+})
 
-    const user = await db.get("SELECT * FROM users WHERE user_mail=? AND user_pw=?", [req.body.mail, req.body.pswd])
-    console.log(user)
+userRouter.post("/login", async (req, res, next) => {
+    const user = await db.get("SELECT * FROM users WHERE user_mail=? AND user_pw=?", [req.body.email, req.body.pswd])
+
     if (user) {
-        console.log("log ind")
-        next();
+        res.status(200).send({ message: `You are already logged in as ${req.body.email}` });
     } else {
-        console.log("wtf")
+        if (user) {
+            next();
+        } else {
+            res.status(400).send({ message: "Wrong email or password" });
+        }
     }
 })
 
