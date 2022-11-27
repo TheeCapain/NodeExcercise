bfbba000a4b78acdfbbcaa0b9172310168a2edcf
August <Augusthauerslev@outlook.com>
2022-11-27 11:09:43 +0100

    def not done

--- Mandatory_2/server/routers/userRouter.js
+++ Mandatory_2/server/routers/userRouter.js
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import db from "../database/connection_sqlite.js"
-import bcrypt, { hash } from 'bcrypt'
-import { nextTick } from 'process';
+import bcrypt from 'bcrypt'
 
 const userRouter = Router()
 const saltRounds = 10;
@@ -32,19 +31,11 @@ userRouter.get("/api/users", async (req, res) => {
 
 userRouter.post("/api/users/login", async (req, res, next) => {
 
-    console.log(req.body.password)
-    console.log(req.body.email)
-
-    const hashedPw = hashPW(req.body.password)
-
     const user = await db.get("SELECT * FROM users WHERE user_mail=?", [req.body.email])
-    console.log(user)
-    console.log(user.user_pw)
-    console.log(hashedPw)
-    if (user) {
-        console.log("session is set");
-        next(user);
+    const istrue = bcrypt.compare(req.body.password, user.user_pw)
 
+    if (istrue) {
+        console.log("session is set");
     } else {
         console.log("test")
         res.status(400).send({ message: "Wrong email or password" });
